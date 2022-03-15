888e373c8f3be4b097492c3c9140eb5e9f699210
Felix Onen <onenfelix90@gmail.com>
2022-03-16 01:27:48 +0300

    Added the expenses module, with its static components

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Card from '../UI/Card';
+import Expensedate from './ExpenseDate';
+import './ExpenseItem.css';
+
+const ExpenseItem = (props) => {
+
+  return (
+    <Card className="expense-item">
+      <Expensedate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
